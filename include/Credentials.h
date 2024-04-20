2f634caacf72d0ec92e3c92f19211431cae1688c
EdwardThandi <thandiedward4@gmail.com>
2024-04-20 21:56:32 +0300

    Update Credentials.h

--- include/Credentials.h
+++ include/Credentials.h
@@ -1,8 +1,8 @@
 #ifndef Credentials_h
 #define Credentials_h
 
-char ssid[] = "TECNO CAMON 18";             // your network SSID (name)
-char pass[] = "edduhsparky";         // your network password
+char ssid[] = "Edduh";             // your network SSID (name)
+char pass[] = "sparky100";         // your network password
 
 char user[]         = "admin";              // MySQL user login username
 char password[]     = "electrospark";          // MySQL user login password
